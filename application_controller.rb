3448ec3a8e024953793ad2a510b3ad7d9876f493
JadynM <jadynmiller04@gmail.com>
2018-08-01 14:17:27 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -4,7 +4,33 @@ Bundler.require
 class MyApp < Sinatra::Base
 
   get '/' do
+    @name="Jadyn"
+    @num1 = 15
+    @num2 = 34
+    @hometown = "Nashville"
+    
     erb :index
   end
+
+get '/pasta' do
+  erb :pasta 
+end 
+
+get '/lacrosse' do
+  erb :lacrosse
+end 
+
+# get '/' do 
+#   @num1 = 15
+# end 
+
+# get '/' do 
+#   @num2 = 34 
+# end 
+
+# get 'total' do 
+# end 
+
+
 
 end
